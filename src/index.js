5ee6852aa83a94948d8973b2667719215c8708c8
Saul Lainez <saullainez@hotmail.es>
2019-09-25 10:35:54 -0600

    Configuracion de cabeceras y CORS

--- src/index.js
+++ src/index.js
@@ -5,6 +5,16 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const path = require('path');
 
+// CORS 
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 app.use(express.static(path.resolve(__dirname, '../public')));
 
 app.use(express.json()); 
