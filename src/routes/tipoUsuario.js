d4f467c8ecf31cc8b1ff7f92cbd06958ced9cf5d
Saúl Laínez <saullainez@hotmail.es>
2020-03-08 18:43:21 -0600

    CRUD  de Tipos de Usuario

--- src/routes/tipoUsuario.js
+++ src/routes/tipoUsuario.js
@@ -9,6 +9,6 @@ app.get("/usuarios/tipos", [verificarAutenticacion,verificarTipoUsuario], tipoUs
 app.get("/tipousuario/count", [verificarAutenticacion,verificarTipoUsuario], tipoUsuarioController.countTipoUsuario)
 app.post("/tipousuario/crear",[verificarAutenticacion,verificarTipoUsuario],tipoUsuarioController.crear)
 app.put("/tipousuario/actualizar", [verificarAutenticacion,verificarTipoUsuario],tipoUsuarioController.actualizarTipoUsuario)
-app.delete("/tipousuario/eliminar", [verificarAutenticacion,verificarTipoUsuario],tipoUsuarioController.eliminarTipoUsuario)
+app.post("/tipousuario/eliminar", [verificarAutenticacion,verificarTipoUsuario],tipoUsuarioController.eliminarTipoUsuario)
 
 module.exports = app;
