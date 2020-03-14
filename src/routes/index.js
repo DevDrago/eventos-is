688e62ebc5bc5e8a7f67c0c2adfc17098af43aeb
Saúl Laínez <saullainez@hotmail.es>
2020-03-13 22:12:40 -0600

    Modificar nombre de actividadasistencia para Deploy

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(require("./eventos"));
 app.use(require("./usuarios"));
 app.use(require("./actividades"));
-app.use(require("./actividadasistencia"));
+app.use(require("./actividadAsistencia"));
 app.use(require("./actividadresponsable"));
 app.use(require("./categoriaActividad"));
 app.use(require("./recursos"));
