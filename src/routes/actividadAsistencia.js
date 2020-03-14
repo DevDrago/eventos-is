5c7d606a99aabcbdfe60da0cedfe11997facbc8e
Saúl Laínez <saullainez@hotmail.es>
2020-03-14 10:48:46 -0600

    Probar ruta en el Deploy de Heroku

--- src/routes/actividadAsistencia.js
+++ src/routes/actividadAsistencia.js
@@ -4,7 +4,9 @@ const {verificarAutenticacion} = require("../middlewares/verificarAutenticacion.
 const {verificarTipoUsuario} = require("../middlewares/verificarTipoUsuario")
 const  actividadAsistenciaController = require("../controllers/actividadAsistenciaController")
 
-
+app.get('/', function(req, res) {
+    res.send('hello world');
+  });
 app.get("/actividadasistencia", [verificarAutenticacion,verificarTipoUsuario], actividadAsistenciaController.actividadAsistencia)
 app.post("/actividadasistencia/crear",[verificarAutenticacion,verificarTipoUsuario],actividadAsistenciaController.crear)
 app.put("/actividadasistencia/actualizar", [verificarAutenticacion,verificarTipoUsuario],actividadAsistenciaController.actualizarAsistenciaActividad)
