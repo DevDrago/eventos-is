9d3e7a6050505cc70070834df99c4f128c9d56f5
Saúl Laínez <saullainez@hotmail.es>
2020-03-14 10:17:48 -0600

    Cambiar actividadresponsable a actividadResponsable para Deploy en Heroku

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ app.use(require("./eventos"));
 app.use(require("./usuarios"));
 app.use(require("./actividades"));
 app.use(require("./actividadAsistencia"));
-app.use(require("./actividadresponsable"));
+app.use(require("./actividadResponsable"));
 app.use(require("./categoriaActividad"));
 app.use(require("./recursos"));
 app.use(require("./tipoRecurso"));
