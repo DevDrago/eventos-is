6af41701185eef2d5ac494272f62203ed32eef74
Saúl Laínez <saullainez@hotmail.es>
2020-03-14 11:22:53 -0600

    Probar configuración para conectarse a la BD en producción

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -9,7 +9,7 @@ const mysql = require("mysql");
 //Configuracion para producción
 module.exports = ()=>{
     return mysql.createConnection({
-        host: 'localhost',
+        host: '185.201.11.233',
         user: 'u137828084_eventos',
         password:"eventos2020",
         database:"u137828084_eventos_is"
